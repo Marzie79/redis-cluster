000200ff58fd706674855c96e5810c173c5b6035
marzie79 <marzie.7900@gmail.com>
2023-04-08 13:06:30 +0330

    Add clustring config for redis

--- redis_cluster/settings.py
+++ redis_cluster/settings.py
@@ -49,7 +49,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'redis_cluster.wsgi.application'
 from redis.cluster import ClusterNode
 
-REDIS_NODES = [ 
+REDIS_NODES = [
     ClusterNode('172.20.0.31', 6373),
     ClusterNode('172.20.0.32', 6374),
     ClusterNode('172.20.0.33', 6375),
@@ -62,7 +62,7 @@ REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", '')
 REDIS_DB = os.environ.get("REDIS_DB", 0)
 REDIS_URI = f'redis://:{REDIS_PASSWORD}@172.20.0.31:6373,172.20.0.32:6374,172.20.0.33:6375,172.20.0.34:6376,172.20.0.35:6377,172.20.0.36:6378'
-
+CELERY_BROKER_URL = 'redis://:@172.20.0.31:6373/0,172.20.0.32:6374/0,172.20.0.33:6375/0,172.20.0.34:6376/0,172.20.0.35:6377/0,172.20.0.36:6378/0'
 
 GLOBAL_CACHE_DB = os.environ.get("GLOBAL_CACHE_DB", 10)
 GLOBAL_CACHE_CONF = {}
@@ -74,7 +74,7 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
-        "KEY_PREFIX": "herlife_community_cache",
+        "KEY_PREFIX": "redis_cluster_cache",
         "TIMEOUT": None
     }
 }
@@ -86,18 +86,23 @@ DATABASES = {
     }
 }
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -112,32 +117,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-# app = Celery('your_app_name')
-# app.conf.broker_transport_options = {'visibility_timeout': 43200}
-# app.conf.broker_connection_retry = True
-# app.conf.broker_connection_max_retries = 3
-# app.conf.broker_url = CELERY_BROKER_URL
-# app.conf.redis_cluster = redis_cluster
-
-# from django.conf import settings
-# from redis.cluster import RedisCluster
-# from redis import ConnectionPool
-
-# CELERY_BROKER_URL = 'redis://:@172.20.0.31:6373/0,172.20.0.32:6374/0,172.20.0.33:6375/0,172.20.0.34:6376/0,172.20.0.35:6377/0,172.20.0.36:6378/0'
-
-# redis_pool = ConnectionPool.from_url(
-#     CELERY_BROKER_URL,
-#     max_connections=20,
-# )
-
-# redis = RedisCluster(
-#     startup_nodes=settings.REDIS_NODES,
-#     moved_response=False,
-#     decode_responses=True,
-# read_from_replicas=True)
-
-# redis.set("aest", "value")
