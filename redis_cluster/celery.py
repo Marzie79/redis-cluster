46f95351099663689fd91e9040ce1cc92de576cd
marzie79 <marzie.7900@gmail.com>
2023-04-08 13:08:27 +0330

    add celery file and redis config for using redis as a broker

---
+++ redis_cluster/celery.py
@@ -0,0 +1,6 @@
+# app = Celery('your_app_name')
+# app.conf.broker_transport_options = {'visibility_timeout': 43200}
+# app.conf.broker_connection_retry = True
+# app.conf.broker_connection_max_retries = 3
+# app.conf.broker_url = CELERY_BROKER_URL
+# app.conf.redis_cluster = redis_cluster
